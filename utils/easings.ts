6d133a2df10cf01d5754ce0ecec982f940efcd12
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-02-22 11:20:35 -0400

    Added easings.ts file

--- utils/easings.ts
+++ utils/easings.ts
@@ -1,11 +1,13 @@
 import { Easing } from 'framer-motion';
 
 export const easings: { [key: string]: Easing } = {
-    easeOutExpo: [0.16, 1, 0.3, 1],
-    easeOutQuint: [0.22, 1, 0.36, 1],
     easeOutCubic: [0.33, 1, 0.68, 1],
     easeOutQuart: [0.25, 1, 0.5, 1],
+    easeOutQuint: [0.22, 1, 0.36, 1],
+    easeOutExpo: [0.16, 1, 0.3, 1],
+
     easeInOutQuart: [0.76, 0, 0.24, 1],
-    easeInQuart: [0.5, 0, 0.75, 0],
     easeInOutCubic: [0.65, 0, 0.35, 1],
+
+    easeInQuart: [0.5, 0, 0.75, 0],
 };
