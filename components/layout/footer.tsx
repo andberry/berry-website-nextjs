a9bdf6cb1c301349019d3d7ea77ab6c71a268cb3
Andrea Berardi <andrea.berardi@verbinteractive.com>
2023-02-26 23:21:08 -0400

    Footer: fixed padding for consistency with header and Container

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 
 export default function Footer() {
     return (
-        <footer className="py-4 px-8 bg-black0 text-white text-sm font-exo2">
+        <footer className="py-4 px-5 md:px-12 bg-black0 text-white text-sm font-exo2">
             Designed and built by Berry (Andrea Berardi) with Next.js and
             TailwindCSS.
         </footer>
